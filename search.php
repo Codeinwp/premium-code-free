0d90b6985b1033e41e3c9e7ef2d04b0db763c76a
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2014-06-28 12:48:10 +0300

    rm blank lines to make it pretty

--- search.php
+++ search.php
@@ -1,9 +1,5 @@
-<?php
-/**
- * The template for displaying Search Results pages.
- *
- * @package premium-code
- */
-get_header();
-if ( get_theme_mod( 'page_layout' ) ) :	$page_layout = get_theme_mod( 'page_layout' );else:	$page_layout = 'select_default';	endif;if ( isset( $page_layout ) && $page_layout != '' ):	switch( $page_layout ):		case 'select_opt1':			get_template_part( '/search-archives-layouts/layout-1col-topimg-sidebar' );			break;		case 'select_opt2':			get_template_part( '/search-archives-layouts/layout-1col-leftimg-sidebar' );			break;			case 'select_opt3':			get_template_part( '/search-archives-layouts/layout-1col-rightimg-sidebar' );			break;			case 'select_opt4':			get_template_part( '/search-archives-layouts/layout-1col-leftimg-nosidebar' );			break;			case 'select_opt5':			get_template_part( '/search-archives-layouts/layout-1col-rightimg-nosidebar' );			break;			case 'select_opt6':			get_template_part( '/search-archives-layouts/layout-2cols-nosidebar' );			break;			case 'select_opt7':			get_template_part( '/search-archives-layouts/layout-2cols-sidebar' );				break;		default:			get_template_part( '/search-archives-layouts/layout-1col-topimg-sidebar' );			break;	endswitch;endif;
+<?php
+/** * The template for displaying Search Results pages. * * @package premium-code */
+get_header();
+if ( get_theme_mod( 'page_layout' ) ) :	$page_layout = get_theme_mod( 'page_layout' );else:	$page_layout = 'select_default';	endif;if ( isset( $page_layout ) && $page_layout != '' ) :	switch( $page_layout ):		case 'select_opt1':			get_template_part( '/search-archives-layouts/layout-1col-topimg-sidebar' );			break;		case 'select_opt2':			get_template_part( '/search-archives-layouts/layout-1col-leftimg-sidebar' );			break;			case 'select_opt3':			get_template_part( '/search-archives-layouts/layout-1col-rightimg-sidebar' );			break;			case 'select_opt4':			get_template_part( '/search-archives-layouts/layout-1col-leftimg-nosidebar' );			break;			case 'select_opt5':			get_template_part( '/search-archives-layouts/layout-1col-rightimg-nosidebar' );			break;			case 'select_opt6':			get_template_part( '/search-archives-layouts/layout-2cols-nosidebar' );			break;			case 'select_opt7':			get_template_part( '/search-archives-layouts/layout-2cols-sidebar' );				break;		default:			get_template_part( '/search-archives-layouts/layout-1col-topimg-sidebar' );			break;	endswitch;endif;
 get_footer(); ?>
